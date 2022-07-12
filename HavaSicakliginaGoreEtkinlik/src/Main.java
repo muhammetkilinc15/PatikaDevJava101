431457ca7fe4a378883e080ec3f4b90136173fd2
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2022-07-12 11:59:22 +0300

    Update Main.java

--- HavaSicakliginaGoreEtkinlik/src/Main.java
+++ HavaSicakliginaGoreEtkinlik/src/Main.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+/*   
+
+      Koşullar :
+* Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+* Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+* Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+* Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+
+       
+ */
+        
+        
+        
         int temp ;
 
         Scanner input = new Scanner(System.in);
