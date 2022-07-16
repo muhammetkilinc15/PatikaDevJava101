5aa1edc76d1f8dcf604505b56109b8d4666892b6
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2022-07-16 21:50:17 +0300

    Add files via upload

---
+++ Girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+        int k;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Sayı giriniz : ");
+        k=inp.nextInt();
+
+
+
+
+
+        for (int i =1 ; i<=k ; i*=4){
+            System.out.println(i + " ");
+        }for (int h =1 ; h<=k ; h*=5){
+            System.out.println(h +" ");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
