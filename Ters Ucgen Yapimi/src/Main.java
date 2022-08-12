3f4f00f85bda4da71fa29c31f0163abd39f97433
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2022-08-12 15:12:56 +0300

    Add files via upload

---
+++ Ters Ucgen Yapimi/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int sayi = klavye.nextInt();
+
+       
+
+
+        for (int i=sayi; i >0 ; i--){
+
+            for (int k=sayi-i; k>0; k--){
+                System.out.print(" ");
+            }
+
+            for (int j=(2*i)-1; j>0; j--){
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
